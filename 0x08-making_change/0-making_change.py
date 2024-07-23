bf3f1f46faff16babcda1e3c3a679ae17451d7bb
mostafazhrn <mostafazhrn@gmail.com>
2024-07-23 17:59:23 -0400

    push_task0_readme_fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" this script shall determine fewest num of coins to make change"""
+""" this script shall determine least num of coins needed to make change"""
 
 
 def makeChange(coins, total):
-    """ THis function shall determine the fewest number of coins needed"""
-    if (type(total) is not int) or type(coins) is not list:
-        return -1
+    """ THis function shall determine fewest number of coins for mk chng"""
     if total <= 0:
         return 0
-    try:
-        less = [float('inf') for i in range(total + 1)]
-        less[0] = 0
-        for x in range(1, total + 1):
-            for y in range(len(coins)):
-                if less[x - coins[y]] + 1 < less[x]:
-                    less[x] = less[x - coins[y]] + 1
-        if less[total] != float('inf'):
-            return less[total]
-        else:
-            return -1
-    except Exception:
+    if len(coins) == 0:
         return -1
+    coins = sorted(coins)
+    dyno = [float('inf')] * (total + 1)
+    dyno[0] = 0
+    for x in range(total + 1):
+        for y in coins:
+            if y > x:
+                break
+            if dyno[x - y] != -1:
+                dyno[x] = min(dyno[x - y] + 1, dyno[x])
+    if dyno[total] == float('inf'):
+        return -1
+    return dyno[total]
